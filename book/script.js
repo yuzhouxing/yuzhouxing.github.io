0e6245430354030cdb972ac0834c6c6b29c1da8e
yuzhouxing <13675800495@163.com>
2026-07-08 21:52:36 +0800

    Update script.js

--- book/script.js
+++ book/script.js
@@ -1,10 +1,8 @@
 // book/script.js
 console.log('初始化Supabase客户端...');
 
-const SUPABASE_URL = 'https://exvsmuoyxmpnnaocoxie.supabase.co';
-// 注意：这里要用你的 anon public key，不是之前的 JWT
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4dnNtdW95eG1wbm5hb2NveGllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1NTY5MTQsImV4cCI6MjA3MjEzMjkxNH0.0tcoiCMaIMI7tdu_Zajg0Dq4kp_qnG-K4r1Ak89g6JM';
-
+const SUPABASE_URL = 'https://seijdhrrdpojbkuwddmq.supabase.co';
+        const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlaWpkaHJyZHBvamJrdXdkZG1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1MDE3OTEsImV4cCI6MjA5OTA3Nzc5MX0.0E1QZXiPS203lltmW6XwrnG4Dd0MhxJpwpI-RDRgqG4';
 // 初始化Supabase客户端
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
